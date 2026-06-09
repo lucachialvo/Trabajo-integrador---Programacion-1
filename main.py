f329d7f10587629d892c37993ade4b5746cca3b6
lucachialvo <lucachialvo@users.noreply.github.com>
2026-06-09 11:54:52 -0300

    Estructura inicial, no confirmada

---
+++ main.py
@@ -0,0 +1,15 @@
+# Agregar un pais al dataset con todos los datos
+
+# Actualizar datos de poblacion y superficie de UN pais
+
+# buscar pais por nombre (coincidencia parcial o exacta)
+
+# filtrar paises por 1) continente, 2) rango poblacion, 3) rango superficie
+
+# ordenar paises por: 1) nombre, 2) poblacion, 3) superficie (ascendente y descendente)
+
+# mostrar estadisticas:
+#   - pais con mayor y menor poblacion
+#   - promedio de poblacion
+#   - promedio de superficie
+#   - cantidad de paises por continente
